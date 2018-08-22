971f4160293f8d524074a84bbc7f96c58cdbb4ab
Klkushin Roman <klushin.roman@gmail.com>
2018-08-22 19:20:11 +0600

    Обновление файла Weather.php

--- src/home_work_2/weather.php
+++ src/home_work_2/weather.php
@@ -1,6 +1,19 @@
-<?php
-    $request = 'http://api.openweathermap.org/data/2.5/weather?APPID=ad0cbfc4817e1f6adf5eb92e02662145&q=Bishkek';
-    $response  = file_get_contents($request);
-    $jsonobj  = json_decode($response);
-    print_r($jsonobj);
-?>
+<?
+
+$url = "http://api.openweathermap.org/data/2.5/weather?APPID=ad0cbfc4817e1f6adf5eb92e02662145&q=Bishkek";
+
+$data = @file_get_contents($url);
+if($data){
+    $json = json_decode($data, false);
+    // print_r ($json->coord);
+    $array = $json;
+    foreach($array as $oneDay){
+        echo "Температура: " . $oneDay->temp . "<br/>"; 
+        echo "Влажность: " . $oneDay->humidity . "<br/>";
+        echo "Давление: " . $oneDay->pressure . "<br/>";
+        echo "Скорость ветра: " . $oneDay->speed . "<br/>";
+                 echo "<hr/>";
+    }
+}else{
+    echo "Сервер не доступен!";
+}
