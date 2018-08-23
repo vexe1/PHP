b4fa66936f5ac0593a7fc0d07614073c393249af
Klkushin Roman <klushin.roman@gmail.com>
2018-08-23 11:00:09 +0600

    Обновлено money.php

--- src/home_work_3/money/money.php
+++ src/home_work_3/money/money.php
@@ -1,9 +1,6 @@
 <?
 var_dump($argv);
-$res = fopen("./test.csv", "w");
-for ($i=0; $i < count($argv) ; $i++) { 
-    $item = $argv[$i];
-    fwrite($res,implode(" - ", $item). "\n");
-}
-fclose($res);
-print_r(file_get_contents("./test.csv"));
+$data = implode(" ", $argv);
+
+print_r($data);
+
