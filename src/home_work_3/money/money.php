064eb171c2b7cfd209243ece85e6ed1dba9f1944
Klkushin Roman <klushin.roman@gmail.com>
2018-08-26 17:40:41 +0600

    ДЗ-3 считает сумму

--- src/home_work_3/money/money.php
+++ src/home_work_3/money/money.php
@@ -1,12 +1,12 @@
 ﻿<?
 var_dump($argv);
 $date = date("d.m.y");
-$today = 
+$amount = 0;
+$myList = "./list.csv";
 
 $output=array_slice($argv, 1);
 $data = implode(" ", $output);
-
-
+// Запись в файл
 $res = fopen("./list.csv", "a");
 fwrite($res, $date. ' '. $data."\r\n");
 if ($res) {
@@ -16,3 +16,21 @@ else {
     echo "Не записано";
 }
 fclose($res);
+//сумма за день
+if ($argv[1] === "--today") {
+    if (file_exists($myList)) {
+        $array = file($myList);
+        for ($i=0; $i < count($array); $i++) { 
+            $money = explode (";", $array[$i]);
+            if ($date === $money[0]) {
+                $amount += $money[1]; 
+            }
+        }            
+        echo "$date расход за день: $amount";
+    } else {
+        echo "Файл не существует";
+    }
+}
+
+
+
