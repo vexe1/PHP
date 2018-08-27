bc4e69368b9c3ef0368df4e5c0b35286eec06d6b
Klkushin Roman <klushin.roman@gmail.com>
2018-08-27 16:01:44 +0600

    Задание money решено

--- src/home_work_3/money/money.php
+++ src/home_work_3/money/money.php
@@ -3,27 +3,32 @@ var_dump($argv);
 $date = date("d.m.y");
 $amount = 0;
 $myList = "./list.csv";
-
 $output=array_slice($argv, 1);
 $data = implode(" ", $output);
+
 // Запись в файл
+if (is_numeric($argv[1]) && (!is_numeric($argv[2])) || ($argv[1] === "--today")) {
 $res = fopen("./list.csv", "a");
 fwrite($res, $date. ' '. $data."\r\n");
+fclose($res);
+} else {
+    echo 'Ошибка! Аргументы не заданы, укажите флаг --today или запустите скрипт с аргументами {цена} и {описание покупки}' . PHP_EOL;
+ }
 if ($res) {
-    echo "Добавлена строка: $date $data";
+    echo "Добавлена строка: $date $data" . PHP_EOL;
 }
 else {
-    echo "Не записано";
+    echo "Не записано" . PHP_EOL;
 }
-fclose($res);
+
 //сумма за день
 if ($argv[1] === "--today") {
     if (file_exists($myList)) {
         $array = file($myList);
         for ($i=0; $i < count($array); $i++) { 
-            $money = explode (";", $array[$i]);
+            $money = explode (" ", $array[$i]);
             if ($date === $money[0]) {
-                $amount += $money[1]; 
+                (int)$amount += (int)$money[1]; 
             }
         }            
         echo "$date расход за день: $amount";
@@ -31,6 +36,3 @@ if ($argv[1] === "--today") {
         echo "Файл не существует";
     }
 }
-
-
-
