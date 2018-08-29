ec2855410b966e524b49bf94d1635d1554bfaced
Klkushin Roman <klushin.roman@gmail.com>
2018-08-29 19:14:00 +0600

    CSV преобразован в массив

--- src/home_work_3/visa/visa.php
+++ src/home_work_3/visa/visa.php
@@ -1,14 +1,9 @@
-<?
-
-$csv = array();
-$lines = file('visa_list.csv', FILE_IGNORE_NEW_LINES);
-foreach ($lines as $key => $value) {
-    $csv[$key] = str_getcsv($value);
-}
-
-foreach ($csv[170] as $one => $second) {
-    if (($one + 1) % 2 == 0) {
-    var_dump ($second);
-
-    }}
-
+<?php
+    $rows   = array_map('str_getcsv', file('visa_list.csv'));
+    $header = array_shift($rows);
+    $csv    = array();
+    foreach($rows as $row) {
+        $csv[] = array_combine($header, $row);
+    }
+    var_dump ($csv);
+?>
